d7a4fb7460dadbccccec58e067da264eb155b64a
Madhu14122 <coolcoder14122@gmail.com>
2023-08-13 12:41:26 +0530

    first-commit

---
+++ src/components/RepositoryItem/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './index.css'
+
+const RepositoryItem = props => {
+  const {repositoryDetails} = props
+  const {
+    name,
+    imageUrl,
+    starsCount,
+    forksCount,
+    issuesCount,
+  } = repositoryDetails
+
+  return (
+    <li className="repository-item">
+      <img src={imageUrl} alt={name} className="repository-image" />
+      <h1 className="repository-name">{name}</h1>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+          alt="stars"
+          className="stars-icon"
+        />
+        <p className="stars-text">{starsCount} stars</p>
+      </div>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+          alt="forks"
+          className="stars-icon"
+        />
+        <p className="stars-text">{forksCount} forks</p>
+      </div>
+      <div className="stars-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png "
+          alt="open issues"
+          className="stars-icon"
+        />
+        <p className="stars-text">{issuesCount} open issues</p>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
